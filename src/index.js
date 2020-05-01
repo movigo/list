d8fc08fb5ff0330c929eb9ee9d3ac9de1faa4ed4
cedoor <me@cedoor.dev>
2020-05-01 16:21:03 +0200

    [feat] add easing function option

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import { addPlugin } from '@movigo/core'
 
-addPlugin(function list(elements, parameters, values) {
+/**
+ * Prepare animation parameters to create a list animation.
+ * @param {Element[] | NodeList} elements
+ * @param {object} parameters
+ * @param {object} values
+ */
+function list (elements, parameters, values) {
   values = {
     x: 0, y: 100,
     duration: .3,
+    easing: 'cubic-bezier(0.0, 0.0, 0.2, 1)',
     ...values
   }
 
@@ -16,4 +23,6 @@ addPlugin(function list(elements, parameters, values) {
   parameters.easing = 'cubic-bezier(0.0, 0.0, 0.2, 1)'
   parameters.delay = Array.from(elements).map((element, i) => i * values.duration / 3)
   parameters.to = { opacity: 1, transform: 'translateY(0px)' }
-})
+}
+
+addPlugin(list)
